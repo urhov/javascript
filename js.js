6d049043a7f54f7e1421f14cb161755ad1b11896
urhov <48583143+urhov@users.noreply.github.com>
2019-05-17 13:39:19 +0300

    s

---
+++ js.js
@@ -0,0 +1,70 @@
+var panos=1;
+var rahaa=75;
+function play ()
+ {
+    myGamePiece = new component (30, 30, "darksalmon", 10, 160);
+    myScore = new component ("40px", "consolas", "black", "black", 280, 40, "text")
+{
+    function updateGameArea (){
+        myGameArea.clear();
+        myGameArea.newPos();
+        myGameArea.update();
+    }
+}
+
+//Pelialueen scripti. 
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 300;
+        this.context = this,canvas.getContext("2d");
+document.body.insertBefore(this.canvas,document.body.childNodes[0])
+
+    }
+}
+    var myGamePiece;
+    var myObstacles = [];
+    var myScore = 50; 
+
+    
+
+
+
+    function startGame () {
+        myGamePiece = new component (30, 30, "blue", 10, 160);
+        myScore =new component ("20px", "consolas", "black", 280, 40, "text")
+        myGameArea.start();
+    
+    }
+{
+        myGamePiece.speedY -= 1;
+    }
+}
+{var myGamePiece;
+
+
+    var myObstacles = [];
+
+}  
+
+{
+    var x = document.createElement("IMG");
+}
+// RGB valot peliin. 
+
+
+const Game = function() {
+this.color = "rgb(0,0,0)";
+this.color = [0, 0, 0,]; 
+this.shifts = [1, 1, 1,];
+
+Game.update();
+}
+   
+ 
+
+
+     
+  
+  
