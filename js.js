1fd2c67bf4d62ef6521a2ad18fcb95b92d2a1d21
myusername <myemail>
2019-08-30 13:30:59 +0300

    tein javascriptiä ja yritin panosta

--- js.js
+++ js.js
@@ -10,6 +10,22 @@ function lukitse(elem) {
         elem.dataset.lock = "false"; 
     }
 }
+// Panos 
+function panos(elem) {
+    console.log(elem.id);
+    if (elem.dataset.change == 'false') {
+        document.getElementById(elem.id)= Panos == 2 
+        elem.dataset.change = "true"; 
 
-    
+}
+else {
+    document.getElementById(elem.id)= Panos == 1
+    elme.dataset.change = "false";
+}
 
+}
+
+
+
+
+// Pelaa napin toiminta
